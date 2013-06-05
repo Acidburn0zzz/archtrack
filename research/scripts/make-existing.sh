78acfb537a160b5d95ea52d1214070fadd8fcf7a
Evan Teitelman <teitelmanevan@gmail.com>
2013-06-05 06:07:02 -0400

    Remove some old research scripts.
    These scripts are entirely useless now.

--- research/scripts/make-existing.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for file in * ; do
-	echo -n $file
-	grep '^Name' $file.names.auri |
-		sed 's/^.*: //'
-done
--- research/scripts/sort-into-categories.sh
+++
@@ -1,7 +0,0 @@
-for cat in {01..14} ; do
-	grep -l "Cats.*$cat-..-" compiled/* |
-	while read file ; do
-		grep '^Pack' $file |
-		sed 's/^.*=//' >> cats/$(grep -m1 "^$cat" ../menu/sections)
-	done 
-done
